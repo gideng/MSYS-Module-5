4164b5e302c5d497cd86c5cedf310dbb32a6fe9a
gideng <gide.alden.ng@student.ateneo.edu>
2026-03-15 19:38:31 +0800

    edited paths in urls.py of MyInventorySystem, basically like the Part 1 Step 1 of the Exercise

--- M5P-DjangoTemplates-AngMangilitNg/MyInventorySystem/urls.py
+++ M5P-DjangoTemplates-AngMangilitNg/MyInventorySystem/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from MyInventoryApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('view_supplier', views.view_supplier, name='view_supplier'),
+    path('view_bottles', views.view_bottles, name='view_bottles'),
+    path('add_bottle', views.add_bottle, name='add_bottle'),
 ]
