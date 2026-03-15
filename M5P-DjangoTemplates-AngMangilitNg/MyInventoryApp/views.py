4e9eea860bc6cd0b0f965fc35679a31f041b86a8
gideng <gide.alden.ng@student.ateneo.edu>
2026-03-15 20:19:22 +0800

    fixed Project base and views.py

--- M5P-DjangoTemplates-AngMangilitNg/MyInventoryApp/views.py
+++ M5P-DjangoTemplates-AngMangilitNg/MyInventoryApp/views.py
@@ -3,11 +3,11 @@ from MyInventoryApp.models import Supplier, WaterBottle
 
 def view_supplier(request):
     supplier_objects = Supplier.objects.all()
-    return render(request, 'MyInventoryApp/suppliers.html', {'suppliers':supplier_objects})
+    return render(request, 'MyInventoryApp/view_supplier.html', {'suppliers':supplier_objects})
 
 def view_bottles(request):
     bottle_objects = WaterBottle.objects.all()
-    return render(request, 'MyInventoryApp/bottles.html', {'bottles':bottle_objects})   
+    return render(request, 'MyInventoryApp/view_bottles.html', {'bottles':bottle_objects})   
 
 def add_bottle(request):
     supplier_objects = Supplier.objects.all()
