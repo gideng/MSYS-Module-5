d1a58d9b849c19e7564d82d4415f43d457d82895
gideng <gide.alden.ng@student.ateneo.edu>
2026-03-15 19:47:14 +0800

    fixed views.py, basically part 1 step 2 of the exercise but since we plan to load data, the add_bottle is slightly different from add_menu

--- M5P-DjangoTemplates-AngMangilitNg/MyInventoryApp/views.py
+++ M5P-DjangoTemplates-AngMangilitNg/MyInventoryApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from MyInventoryApp.models import Supplier, WaterBottle
 
-# Create your views here.
+def view_supplier(request):
+    supplier_objects = Supplier.objects.all()
+    return render(request, 'MyInventoryApp/suppliers.html', {'suppliers':supplier_objects})
+
+def view_bottles(request):
+    bottle_objects = WaterBottle.objects.all()
+    return render(request, 'MyInventoryApp/bottles.html', {'bottles':bottle_objects})   
+
+def add_bottle(request):
+    supplier_objects = Supplier.objects.all()
+    return render(request, 'MyInventoryApp/add_bottle.html', {'suppliers': supplier_objects})    
