c580d9dec78f9af59d03a771a06f4ce58d61889a
Anjou Ang <jobelle.ang@student.ateneo.edu>
2026-03-15 22:26:34 +0800

    Change html file names, and changed them accordingly at urls.py, views.py

--- M5P-DjangoTemplates-AngMangilitNg/MyInventoryApp/urls.py
+++ M5P-DjangoTemplates-AngMangilitNg/MyInventoryApp/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('view_supplier', views.view_supplier, name='view_supplier'),
+    path('', views.view_base, name='view_base'),
+    path('view_suppliers', views.view_suppliers, name='view_suppliers'),
     path('view_bottles', views.view_bottles, name='view_bottles'),
-    path('add_bottle', views.add_bottle, name='add_bottle'),
+    path('add_bottles', views.add_bottles, name='add_bottles'),
 ]
--- M5P-DjangoTemplates-AngMangilitNg/MyInventoryApp/views.py
+++ M5P-DjangoTemplates-AngMangilitNg/MyInventoryApp/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from MyInventoryApp.models import Supplier, WaterBottle
 
-def view_supplier(request):
+def view_base(request):
+    return render(request, 'MyInventoryApp/base.html')
+
+def view_suppliers(request):
     supplier_objects = Supplier.objects.all()
-    return render(request, 'MyInventoryApp/view_supplier.html', {'suppliers':supplier_objects})
+    return render(request, 'MyInventoryApp/view_suppliers.html', {'suppliers':supplier_objects})
 
 def view_bottles(request):
     bottle_objects = WaterBottle.objects.all()
     return render(request, 'MyInventoryApp/view_bottles.html', {'bottles':bottle_objects})   
 
-def add_bottle(request):
+def add_bottles(request):
     supplier_objects = Supplier.objects.all()
-    return render(request, 'MyInventoryApp/add_bottle.html', {'suppliers': supplier_objects})    
+    return render(request, 'MyInventoryApp/add_bottles.html', {'suppliers': supplier_objects})    
