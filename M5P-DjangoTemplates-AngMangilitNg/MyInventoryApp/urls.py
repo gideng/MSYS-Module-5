9fbc3188593cdac432ece2e3ca53f9674e82d9fd
gideng <gide.alden.ng@student.ateneo.edu>
2026-03-16 23:03:27 +0800

    added in-code cert

--- M5P-DjangoTemplates-AngMangilitNg/MyInventoryApp/urls.py
+++ M5P-DjangoTemplates-AngMangilitNg/MyInventoryApp/urls.py
@@ -1,19 +1,19 @@
 """
-URL configuration for MyInventorySystem project.
+Jobelle Ang, 240242
+Sebastian Mangilit, 242880
+Gide Ng, 243225
+16 March 2026
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+We hereby attest to the truth of the following facts:
+We have not discussed the Python code in my program with anyone 
+other than our instructor or the teaching assistants assigned to this course.
+We have not used Python code obtained from another student, 
+or any other unauthorized source, whether modified or unmodified.
+If any Python code or documentation used in my program was 
+obtained from another source, it has been clearly noted with 
+citations in the comments of our program.
 """
+
 from django.urls import path
 from . import views
 
--- M5P-DjangoTemplates-AngMangilitNg/MyInventoryApp/views.py
+++ M5P-DjangoTemplates-AngMangilitNg/MyInventoryApp/views.py
@@ -1,3 +1,19 @@
+"""
+Jobelle Ang, 240242
+Sebastian Mangilit, 242880
+Gide Ng, 243225
+16 March 2026
+
+We hereby attest to the truth of the following facts:
+We have not discussed the Python code in my program with anyone 
+other than our instructor or the teaching assistants assigned to this course.
+We have not used Python code obtained from another student, 
+or any other unauthorized source, whether modified or unmodified.
+If any Python code or documentation used in my program was 
+obtained from another source, it has been clearly noted with 
+citations in the comments of our program.
+"""
+
 from django.shortcuts import render
 from MyInventoryApp.models import Supplier, WaterBottle
 
